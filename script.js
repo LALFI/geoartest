0bd4df8af910c2c451a83272ad2e2923d5a282dd
LALFI <61983662+LALFI@users.noreply.github.com>
2020-03-09 15:37:11 -0300

    change js

--- script.js
+++ script.js
@@ -6,14 +6,69 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: 'Magnemite',
+            name: 'CIOp',
             location: {
                 lat: -34.886738,
                 lng: -58.006384,
-            }
+            }             
+        },
+         {
+            name: 'CETMIC',
+            location: {
+                lat: -34.886393,
+                lng: -58.007050,
+            }             
+        },
+         {
+            name: 'Entrada',
+            location: {
+                lat: -34.884674,
+                lng: -58.004883,
+            }             
         },
     ];
 }
+
+var models = [
+    {
+        url: './assets/magnemite/scene.gltf',
+        scale: '0.15 0.15 0.15',
+        info: 'Magnemite, Lv. 5, HP 10/10',
+        rotation: '0 180 0',
+    },
+    {
+        url: './assets/articuno/scene.gltf',
+        scale: '0.2 0.2 0.2',
+        rotation: '0 180 0',
+        info: 'Articuno, Lv. 80, HP 100/100',
+    },
+    {
+        url: './assets/dragonite/scene.gltf',
+        scale: '0.08 0.08 0.08',
+        rotation: '0 180 0',
+        info: 'Dragonite, Lv. 99, HP 150/150',
+    },
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
+
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
 
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
@@ -24,10 +79,11 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 0 0');         
+        setModel(models[modelIndex], model);
+        //model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        //model.setAttribute('rotation', '0 0 0');         
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.15 0.15 0.15');
+        //model.setAttribute('scale', '0.15 0.15 0.15');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
